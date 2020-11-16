277f475bff1b140628b3532b6b29c9caf01e1195
Jake Weston <jweston491@gmail.com>
2020-11-16 10:04:49 -0800

    Add cpb-figure class to figure element

--- lib/shortcodes/image/image.php
+++ lib/shortcodes/image/image.php
@@ -2,7 +2,7 @@
 
 ?><div class="cpb-image"> 
 	<?php if ( ! empty( $caption ) ) : ?>
-	<figure id="attachment_<?php echo esc_attr( $atts['img_id'] ) ?>" aria-describedby="caption-attachment-<?php echo esc_attr( $atts['img_id'] ) ?>">
+	<figure class="cpb-figure" id="attachment_<?php echo esc_attr( $atts['img_id'] ) ?>" aria-describedby="caption-attachment-<?php echo esc_attr( $atts['img_id'] ) ?>">
 	<?php endif; ?>
 	<?php if ( ! empty( $url ) ) : ?><a href="<?php echo esc_url( $url ); ?>"><?php endif; ?>
 	<img src="<?php echo esc_url( $img_src ); ?>" style="width:100%;display:block" <?php if ( ! empty( $alt ) ) : ?>alt="<?php echo esc_html( $alt ); ?>"<?php endif; ?> /> 
